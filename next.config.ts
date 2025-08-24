1f684b4eb8bcd67af76fb3435e580425aa46253f
Pasindu Umayanga <pasinduumayanga93@gmail.com>
2025-08-25 02:49:26 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
+const repo = "NextJsSample";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // writes static site to ./out on build
   images: { unoptimized: true }, // Pages can't run Next/Image optimizer
   // If deploying to https://USERNAME.github.io/REPO:
-  basePath: "/NextJsSample",
-  assetPrefix: "/NextJsSample/",
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : "",
 };
 
 module.exports = nextConfig;
