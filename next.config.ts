4c2a07742681953cdce9d0271d89f97b4fb1d5ab
Pasindu Umayanga <pasinduumayanga93@gmail.com>
2025-08-25 02:05:16 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // writes static site to ./out on build
+  images: { unoptimized: true }, // Pages can't run Next/Image optimizer
+  // If deploying to https://USERNAME.github.io/REPO:
+  // basePath: '/REPO',
+  // assetPrefix: '/REPO/',
 };
 
 export default nextConfig;
