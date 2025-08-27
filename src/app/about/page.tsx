1d75676b630b36e32f59fb69a5e54924ef95d223
PasinduUmayanga <pasinduumayanga93@gmail.com>
2025-08-27 15:49:34 +0530

    test review

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,8 @@ export default function AboutPage() {
     <div>
       <h1>About Page</h1>
       <p>Welcome to the About page!</p>
-      <p>Test</p>
+      <p>Test sample</p>
     </div>
   );
 }
+https://www.youtube.com/watch?v=qA3IcQrIcgo&list=RDqA3IcQrIcgo&start_radio=1&ab_channel=HiruFM
